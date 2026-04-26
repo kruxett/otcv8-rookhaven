23bcc7e90f62b871cd7d35cc64610e7e36ac02a3
kruxett <ombergmarcus@gmail.com>
2026-04-26 09:21:57 +0200

    Add ping display functionality; implement visibility toggle and update logic

--- modules/game_stats/stats.lua
+++ modules/game_stats/stats.lua
@@ -7,6 +7,10 @@ function init()
   if not modules.client_options.getOption("showFps") then
     ui.fps:hide()
   end
+
+  if ui.ping and not modules.client_options.getOption("showPing") then
+    ui.ping:hide()
+  end
   
   updateEvent = scheduleEvent(update, 200)
 end
@@ -21,6 +25,34 @@ function update()
 
   text = 'FPS: ' .. g_app.getFps()
   ui.fps:setText(text)
+
+  if ui.ping then
+    local pingText = 'PING: --'
+    local pingColor = '#ffffff'
+    if g_game.isOnline() then
+      if g_game.getFeature(GameClientPing) or g_game.getFeature(GameExtendedClientPing) then
+        local ping = tonumber(g_game.getPing()) or -1
+        if ping >= 0 then
+          pingText = string.format('PING: %d ms', ping)
+          if ping >= 300 then
+            pingColor = '#d97a7a'
+          elseif ping >= 150 then
+            pingColor = '#e0b070'
+          else
+            pingColor = '#7ac97a'
+          end
+        else
+          pingText = 'PING: ...'
+          pingColor = '#e0b070'
+        end
+      else
+        pingText = 'PING: N/A'
+        pingColor = '#9e9e9e'
+      end
+    end
+    ui.ping:setText(pingText)
+    ui.ping:setColor(pingColor)
+  end
 end
 
 function show()
